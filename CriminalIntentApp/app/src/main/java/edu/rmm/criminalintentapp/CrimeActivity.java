cd574c408eee837caa12e4323b945594373de1e1
REMEK <r.m.l.marciniak@gmail.com>
2020-04-29 11:37:06 +0200

    fragment addded to main activity

--- CriminalIntentApp/app/src/main/java/edu/rmm/criminalintentapp/CrimeActivity.java
+++ CriminalIntentApp/app/src/main/java/edu/rmm/criminalintentapp/CrimeActivity.java
@@ -1,6 +1,8 @@
 package edu.rmm.criminalintentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit();
+        }
+
     }
 }
